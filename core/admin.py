a0bf3c0ed46984a7a65a1829ea019a08c2969a1f
Eugene Prikazchikov <eprikazc@gmail.com>
2016-10-16 08:59:09 +0300

    Draft version

---
+++ core/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+
+from .models import Proc, Bread, Hat, Money, Doc, Person
+
+
+class BreadInline(admin.TabularInline):
+    model = Bread
+    extra = 1
+
+
+class HatInline(admin.TabularInline):
+    model = Hat
+    extra = 1
+
+
+class MoneyInline(admin.TabularInline):
+    model = Money
+    extra = 1
+
+
+class ProcAdmin(admin.ModelAdmin):
+    inlines = [
+        BreadInline, HatInline, MoneyInline
+    ]
+
+
+admin.site.register(Proc, ProcAdmin)
+admin.site.register([Bread, Hat, Money, Doc, Person])
---
+++ core/models.py
@@ -0,0 +1,77 @@
+from django.db import models
+
+
+class Proc(models.Model):
+    name = models.CharField(max_length=30)
+    state = models.CharField(max_length=30, blank=True)
+    address = models.CharField(max_length=255, blank=True)
+    phone = models.CharField(max_length=30, blank=True)
+    start_date = models.DateField()
+    status = models.BooleanField()
+    last_updated = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Bread(models.Model):
+    address = models.CharField(max_length=255)
+    phone = models.CharField(max_length=30)
+    start_term = models.DateField()
+    end_term = models.DateField()
+    proc = models.ForeignKey(Proc)
+
+    def __str__(self):
+        return str(self.proc)
+
+
+class Hat(models.Model):
+    long_id = models.CharField(max_length=30, unique=True)
+    grant = models.CharField(max_length=30)
+    status = models.CharField(max_length=30)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    issue_date = models.DateField()
+    proc = models.ForeignKey(Proc)
+
+    def __str__(self):
+        return '%s, %s' % (self.proc, self.long_id)
+
+
+class Money(models.Model):
+    class Meta:
+        verbose_name_plural = "money"
+    month = models.CharField(max_length=30)
+    gains = models.DecimalField(max_digits=7, decimal_places=2)
+    losses = models.DecimalField(max_digits=7, decimal_places=2)
+    checked = models.BooleanField()
+    link = models.URLField(blank=True)
+    proc = models.ForeignKey(Proc)
+
+    def __str__(self):
+        return '%s, %s' % (self.proc, self.month)
+
+
+class Doc(models.Model):
+    long_id = models.CharField(max_length=30, unique=True)
+    effective = models.DateField()
+    expires = models.DateField()
+    checked = models.BooleanField()
+    link = models.URLField(blank=True)
+    proc = models.ForeignKey(Proc)
+
+    def __str__(self):
+        return '%s, %s' % (self.proc, self.long_id)
+
+
+class Person(models.Model):
+    class Meta:
+        verbose_name_plural = "people"
+    name = models.CharField(max_length=30)
+    address = models.CharField(max_length=255)
+    phone = models.CharField(max_length=30)
+    title = models.CharField(max_length=30)
+    notes = models.TextField()
+    docs = models.ManyToManyField(Doc, blank=True)
+
+    def __str__(self):
+        return self.name
